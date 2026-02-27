e38280e98890f565463c4edbd6987c06f2e50737
Axel Flordal <axel@flordal.net>
2026-02-27 13:29:45 +0100

    Add style changes and update changed function name

--- tests/Prop/Matrix.hs
+++ tests/Prop/Matrix.hs
@@ -3,7 +3,7 @@ module Prop.Matrix (tests) where
 import Linear.Epsilon (nearZero)
 import Linear.Matrix (M22, det22, inv22, transpose, (!*!), (!+!))
 import Prop.V2 ()
-import Prop.Vector (additive_assoc)
+import Prop.Vector (additiveAssoc)
 import Test.QuickCheck (Property, (==>))
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.QuickCheck (testProperty)
@@ -18,10 +18,10 @@ prop_m22addcommut :: M22 Double -> M22 Double -> Bool
 prop_m22addcommut a b = nearZero ((a + b) - (b + a))
 
 prop_m22addassoc :: M22 Double -> M22 Double -> M22 Double -> Bool
-prop_m22addassoc = additive_assoc (!+!)
+prop_m22addassoc = additiveAssoc (!+!)
 
 prop_m22multassoc :: M22 Double -> M22 Double -> M22 Double -> Bool
-prop_m22multassoc = additive_assoc (!*!)
+prop_m22multassoc = additiveAssoc (!*!)
 
 prop_m22invmult :: M22 Double -> M22 Double -> Property
 prop_m22invmult a b = (det22 a /= 0 && det22 b /= 0) ==> nearZero $ inv22 (a !*! b) - (inv22 b !*! inv22 a)
