0609298213ae2cb575c0371481f636d672465500
Axel Flordal <axel@flordal.net>
2026-02-28 14:09:36 +0100

    Swap to matrix specific operators

--- tests/Prop/Matrix.hs
+++ tests/Prop/Matrix.hs
@@ -1,7 +1,7 @@
 module Prop.Matrix (tests) where
 
 import Linear.Epsilon (nearZero)
-import Linear.Matrix (M22, det22, inv22, transpose, (!*!), (!+!))
+import Linear.Matrix (M22, det22, inv22, transpose, (!*!), (!+!), (!-!))
 import Prop.V2 ()
 import Prop.Vector (additiveAssoc)
 import Test.QuickCheck (Property, (==>))
@@ -15,7 +15,7 @@ prop_m22transpose :: M22 Double -> Bool
 prop_m22transpose a = transpose (transpose a) == a
 
 prop_m22addcommut :: M22 Double -> M22 Double -> Bool
-prop_m22addcommut a b = nearZero ((a + b) - (b + a))
+prop_m22addcommut a b = nearZero ((a !+! b) !-! (b !+! a))
 
 prop_m22addassoc :: M22 Double -> M22 Double -> M22 Double -> Bool
 prop_m22addassoc = additiveAssoc (!+!)
@@ -24,7 +24,7 @@ prop_m22multassoc :: M22 Double -> M22 Double -> M22 Double -> Bool
 prop_m22multassoc = additiveAssoc (!*!)
 
 prop_m22invmult :: M22 Double -> M22 Double -> Property
-prop_m22invmult a b = (det22 a /= 0 && det22 b /= 0) ==> nearZero $ inv22 (a !*! b) - (inv22 b !*! inv22 a)
+prop_m22invmult a b = (det22 a /= 0 && det22 b /= 0) ==> nearZero $ inv22 (a !*! b) !-! (inv22 b !*! inv22 a)
 
 tests :: [TestTree]
 tests =
@@ -32,9 +32,9 @@ tests =
       "2x2 matrix"
       [ testProperty "inv22 (inv22 a) == a" prop_m22inv
       , testProperty "transpose (transpose a) == a" prop_m22transpose
-      , testProperty "commutativity of +" prop_m22addcommut
-      , testProperty "associativity of +" prop_m22addassoc
-      , testProperty "associativity of *" prop_m22multassoc
+      , testProperty "commutativity of !+!" prop_m22addcommut
+      , testProperty "associativity of !+!" prop_m22addassoc
+      , testProperty "associativity of !*!" prop_m22multassoc
       , testProperty "(AB)^-1 == B^-1 * A^-1" prop_m22invmult
       ]
   ]
