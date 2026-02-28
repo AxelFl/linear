129188f9ea32cc56f01a4b7946a06313ae956828
Axel Flordal <axel@flordal.net>
2026-02-28 14:29:46 +0100

    Add test for matrix identity

--- tests/Prop/Matrix.hs
+++ tests/Prop/Matrix.hs
@@ -1,8 +1,9 @@
 module Prop.Matrix (tests) where
 
 import Linear.Epsilon (nearZero)
-import Linear.Matrix (M22, det22, inv22, transpose, (!*!), (!+!), (!-!))
+import Linear.Matrix (M22, det22, inv22, M33, transpose, (!*!), (!+!), (!-!), identity)
 import Prop.V2 ()
+import Prop.V3 ()
 import Prop.Vector (additiveAssoc)
 import Test.QuickCheck (Property, (==>))
 import Test.Tasty (TestTree, testGroup)
@@ -26,14 +27,19 @@ prop_m22multassoc = additiveAssoc (!*!)
 prop_m22invmult :: M22 Double -> M22 Double -> Property
 prop_m22invmult a b = (det22 a /= 0 && det22 b /= 0) ==> nearZero $ inv22 (a !*! b) !-! (inv22 b !*! inv22 a)
 
+prop_m33multident :: M33 Double -> Bool
+prop_m33multident a = a !*! identity == a
+
 tests :: [TestTree]
 tests =
   [ testGroup
+      -- These tests don't rely on any specific size of matrix to function
       "general matrix operations"
       [ testProperty "transpose (transpose a) == a" prop_m22transpose
       , testProperty "commutativity of !+!" prop_m22addcommut
       , testProperty "associativity of !+!" prop_m22addassoc
       , testProperty "associativity of !*!" prop_m22multassoc
+      , testProperty "identity matrix neutral element under !*!" prop_m33multident
       ]
   , testGroup
       "2x2 matrix"
