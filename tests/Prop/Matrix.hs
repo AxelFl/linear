a1a2c0c5f7ea64dd97a6e5dce43582791b7d19d9
Axel Flordal <axel@flordal.net>
2026-03-01 13:31:14 +0100

    Add more matrix tests

--- tests/Prop/Matrix.hs
+++ tests/Prop/Matrix.hs
@@ -1,7 +1,7 @@
 module Prop.Matrix (tests) where
 
 import Linear.Epsilon (nearZero)
-import Linear.Matrix (M22, det22, inv22, M33, transpose, (!*!), (!+!), (!-!), identity, Trace(trace))
+import Linear.Matrix (M22, det22, inv22, M33, transpose, (!*!), (!+!), (!-!), identity, Trace(trace), (!!*), (*!!))
 import Prop.V2 ()
 import Prop.V3 ()
 import Prop.Vector (additiveAssoc)
@@ -31,7 +31,9 @@ prop_m22multassoc = additiveAssoc (!*!)
 prop_m22invmult :: M22 Double -> M22 Double -> Property
 prop_m22invmult a b = (not . (>) 1 . det22) a && (not . (>) 1 . det22) b ==>
   nearZero (inv22 (a !*! b) !-! (inv22 b !*! inv22 a))
- 
+
+prop_m22invident :: M22 Double -> Property
+prop_m22invident a = det22 a /= 0 ==> nearZero (a !*! inv22 a !-! identity)
 
 prop_m33multident :: M33 Double -> Bool
 prop_m33multident a = a !*! identity == a
@@ -41,6 +43,9 @@ prop_tracelinear a b = nearZero (trace (a !+! b) - (trace a + trace b))
 
 prop_tracetranspose :: M33 Double -> Bool
 prop_tracetranspose a = trace a == trace (transpose a)
+
+prop_m33lrscalar :: M33 Double -> Double -> Bool
+prop_m33lrscalar m a = m !!* a == a *!! m
 
 tests :: [TestTree]
 tests =
@@ -54,10 +59,12 @@ tests =
       , testProperty "identity matrix neutral element under !*!" prop_m33multident
       , testProperty "trace (a !+! b) == trace a + trace b" prop_tracelinear
       , testProperty "trace a == trace (transpose a)" prop_tracetranspose
+      , testProperty "Left and right scalar product are equal" prop_m33lrscalar
       ]
   , testGroup
       "2x2 matrix"
       [ testProperty "inv22 (inv22 a) == a" prop_m22inv
+      , testProperty "a !*! inv a == I" prop_m22invident
       --, testProperty "(AB)^-1 == B^-1 * A^-1" prop_m22invmult
       ]
   ]
