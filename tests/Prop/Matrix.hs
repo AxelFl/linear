2c2d693fb6025470c71a83085b93281b25a93d17
Anton Nordbeck <anton.nordbeck@gmail.com>
2026-03-01 21:54:17 +0100

    added a new way to do typegeneric testing on matrixes

--- tests/Prop/Matrix.hs
+++ tests/Prop/Matrix.hs
@@ -1,10 +1,10 @@
 module Prop.Matrix (tests) where
 
 import Linear.Matrix (M22, det22, inv22, M33, transpose, (!*!), (!+!), identity, Trace(trace), (!!*), (*!!))
+import Linear.Vector (Additive)
 import Prop.V2 ()
 import Prop.V3 ()
-import Prop.Vector (additiveAssoc)
-import Test.QuickCheck (Property, (==>))
+import Test.QuickCheck (Property, (==>), (.&&.))
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.QuickCheck (testProperty)
 
@@ -14,14 +14,45 @@ prop_m22inv a = (det22 a /= 0) ==> inv22 (inv22 a) == a
 prop_m22transpose :: M22 Rational -> Bool
 prop_m22transpose a = transpose (transpose a) == a
 
-prop_m22addcommut :: M22 Rational -> M22 Rational -> Bool
-prop_m22addcommut a b = (a !+! b) == (b !+! a)
+testsAddAssoc :: TestTree
+testsAddAssoc = testGroup "associativity of !+!" [
+     testProperty "m22" prop_m22addassoc
+    ,testProperty "m33" prop_m33addassoc
+  ]
+  where
+      prop_addassoc:: (Additive m, Additive n, Ord (m (n Rational))) => m (n Rational) -> m (n Rational) -> m (n Rational) -> Bool
+      prop_addassoc a b c = ((a !+! b) !+! c) == (a !+! (b !+! c))
 
-prop_m22addassoc :: M22 Rational -> M22 Rational -> M22 Rational -> Bool
-prop_m22addassoc = additiveAssoc (!+!)
+      prop_m22addassoc :: M22 Rational -> M22 Rational -> M22 Rational -> Bool
+      prop_m22addassoc = prop_addassoc
+      prop_m33addassoc :: M33 Rational -> M33 Rational -> M33 Rational -> Bool
+      prop_m33addassoc = prop_addassoc
 
-prop_m22multassoc :: M22 Rational -> M22 Rational -> M22 Rational -> Bool
-prop_m22multassoc = additiveAssoc (!*!)
+testsMulAssoc :: TestTree
+testsMulAssoc = testGroup "associativity of !*!" [
+     testProperty "m22" prop_m22mulassoc
+    ,testProperty "m33" prop_m33mulassoc
+  ]
+  where
+    prop_mulassoc:: (Additive m, Foldable m, Ord (m (m Rational))) => m (m Rational) -> m (m Rational) -> m (m Rational) -> Bool
+    prop_mulassoc a b c = ((a !*! b) !*! c) == (a !*! (b !*! c))
+    prop_m22mulassoc :: M22 Rational -> M22 Rational -> M22 Rational -> Bool
+    prop_m22mulassoc = prop_mulassoc
+    prop_m33mulassoc :: M33 Rational -> M33 Rational -> M33 Rational -> Bool
+    prop_m33mulassoc = prop_mulassoc
+
+testsAddCommut :: TestTree
+testsAddCommut = testGroup "commutativity of !+!" [
+     testProperty "m22" prop_m22addcommut
+    ,testProperty "m33"prop_m33addcommut
+  ]
+  where
+    prop_addcommut:: (Additive m, Additive n, Ord (m (n Rational))) => m (n Rational) -> m (n Rational) -> Bool
+    prop_addcommut a b = (a !+! b) == (b !+! a)
+    prop_m22addcommut :: M22 Rational -> M22 Rational -> Bool
+    prop_m22addcommut = prop_addcommut
+    prop_m33addcommut :: M33 Rational -> M33 Rational -> Bool
+    prop_m33addcommut = prop_addcommut
 
 prop_m22invmult :: M22 Rational -> M22 Rational -> Property
 prop_m22invmult a b = det22 a /= 0 && det22 b /= 0 ==>
@@ -45,15 +76,17 @@ prop_m33lrscalar m a = m !!* a == a *!! m
 prop_m33traceswap :: M33 Rational -> M33 Rational -> Bool
 prop_m33traceswap a b = trace (a !*! b) == trace (b !*! a)
 
+
+
 tests :: [TestTree]
 tests =
   [ testGroup
       -- These tests don't rely on any specific size of matrix to function
       "general matrix operations"
       [ testProperty "transpose (transpose a) == a" prop_m22transpose
-      , testProperty "commutativity of !+!" prop_m22addcommut
-      , testProperty "associativity of !+!" prop_m22addassoc
-      , testProperty "associativity of !*!" prop_m22multassoc
+      , testsAddCommut
+      , testsAddAssoc
+      , testsMulAssoc
       , testProperty "identity matrix neutral element under !*!" prop_m33multident
       , testProperty "trace (a !+! b) == trace a + trace b" prop_tracelinear
       , testProperty "trace a == trace (transpose a)" prop_tracetranspose
