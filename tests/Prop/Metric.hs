e0d5ffe49c73ff27bdb67778a746082e99a68fe8
Axel Flordal <axel@flordal.net>
2026-03-02 20:21:45 +0100

    Move metric to Macro style

--- tests/Prop/Metric.hs
+++ tests/Prop/Metric.hs
@@ -1,43 +1,53 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE CPP #-}
+
 module Prop.Metric (tests) where
 
-import Linear.Metric (dot, quadrance)
+import Linear.Metric (Metric, dot, quadrance)
 import Linear.V1 (V1(..))
 import Linear.V2 (V2(..))
 import Linear.V3 (V3(..))
 import Linear.V4 (V4(..))
+import Linear.Vector ((^+^))
 import Prop.V1 ()
 import Prop.V2 ()
 import Prop.V3 ()
 import Prop.V4 ()
-import Test.Tasty (TestTree, testGroup)
+import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck (testProperty)
+import Test.QuickCheck ((.&&.))
 
-prop_dotself :: V3 Rational -> Bool
-prop_dotself a =  a `dot` a == quadrance a
+#define ALLVECTORS(testname) \
+( prop @V1 @Rational .&&. \
+  prop @V2 @Rational .&&. \
+  prop @V3 @Rational .&&. \
+  prop @V4 @Rational \
+)
+
+testsDotSelf :: TestTree
+testsDotSelf = testProperty "dot product is norm squared"
+  ALLVECTORS(prop)
+  where
+    prop :: (Metric v, Num a, Eq a) => v a -> Bool
+    prop a = a `dot` a == quadrance a
 
 testsDotCommut :: TestTree
-testsDotCommut = testGroup "Commutativity of scalar product"
-  [ testProperty "V1" (prop :: V1 Rational -> V1 Rational -> Bool)
-  , testProperty "V2" (prop :: V2 Rational -> V2 Rational -> Bool)
-  , testProperty "V3" (prop :: V3 Rational -> V3 Rational -> Bool)
-  , testProperty "V4" (prop :: V4 Rational -> V4 Rational -> Bool)
-  ]
+testsDotCommut = testProperty "Commutativity of scalar product"
+  ALLVECTORS(prop)
   where
+    prop :: (Metric v, Num a, Eq a) => v a -> v a -> Bool
     prop a b = a `dot` b == b `dot` a
 
 testsDotDist :: TestTree
-testsDotDist = testGroup "Distributivity of scalar product over addition"
-  [ testProperty "V1" (prop :: V1 Rational -> V1 Rational -> V1 Rational -> Bool)
-  , testProperty "V2" (prop :: V2 Rational -> V2 Rational -> V2 Rational -> Bool)
-  , testProperty "V3" (prop :: V3 Rational -> V3 Rational -> V3 Rational -> Bool)
-  , testProperty "V4" (prop :: V4 Rational -> V4 Rational -> V4 Rational -> Bool)
-  ]
+testsDotDist = testProperty "Distributivity of scalar product over addition"
+  ALLVECTORS(prop)
   where
-    prop a b c = (a+b) `dot` c == (a `dot` c) + (b `dot` c) 
+    prop :: (Metric v, Num a, Eq a) => v a -> v a -> v a -> Bool
+    prop a b c = (a ^+^ b) `dot` c == (a `dot` c) + (b `dot` c) 
 
 tests :: [TestTree]
 tests =
-  [ testProperty "dot product is norm squared" prop_dotself
+  [ testsDotSelf
   , testsDotCommut
   , testsDotDist
   ]
