e6626197216393432f121ada9234c7e1e4f63f71
Axel Flordal <axel@flordal.net>
2026-03-02 22:48:23 +0100

    Convert Vector

--- tests/Prop/Vector.hs
+++ tests/Prop/Vector.hs
@@ -14,66 +14,60 @@ import Prop.V3 ()
 import Prop.V4 ()
 import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck (testProperty)
-import Test.QuickCheck ((.&&.))
+import Test.QuickCheck ((.&&.), Property)
 
 -- This macro avoids having these lines in all tests that
 -- want to run on all vectors. It is a bit evil but we think
 -- this is the simplest solution.
-#define TESTALLVECTORS(testname) \
+#define ALLVECTORS(testname) \
 ( testname @V1 @Rational .&&. \
   testname @V2 @Rational .&&. \
   testname @V3 @Rational .&&. \
   testname @V4 @Rational \
 )
 
-testsAddAssoc :: TestTree
-testsAddAssoc = testProperty "associativity of ^+^"
-  TESTALLVECTORS(prop)
+prop_AddAssoc :: Property
+prop_AddAssoc = ALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> v a -> Bool
     prop a b c = ((a ^+^ b) ^+^ c) == (a ^+^ (b ^+^ c))
 
-testsAddCommut :: TestTree
-testsAddCommut = testProperty "commutativity of ^+^"
-  TESTALLVECTORS(prop)
+prop_AddCommut :: Property
+prop_AddCommut = ALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> Bool
     prop a b = (a ^+^ b) == (b ^+^ a)
 
-testsLRScalarProduct :: TestTree
-testsLRScalarProduct = testProperty "Left and right scalar product are equal"
-  TESTALLVECTORS(prop)
+prop_LRScalarProduct :: Property
+prop_LRScalarProduct = ALLVECTORS(prop)
   where
     prop :: (Eq (v a), Functor v, Num a) => v a -> a -> Bool
     prop v a = v ^* a == a *^ v
 
-testsDistScalarR :: TestTree
-testsDistScalarR = testProperty "Right scalar are distributive over vector addition"
-  TESTALLVECTORS(prop)
+prop_DistScalarR :: Property
+prop_DistScalarR = ALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> a -> Bool
     prop a b c = (a ^+^ b) ^* c == (a^*c) ^+^ (b^*c)
 
-testsDistScalarL :: TestTree
-testsDistScalarL = testProperty "Left scalar are distributive over vector addition"
-  TESTALLVECTORS(prop)
+prop_DistScalarL :: Property
+prop_DistScalarL = ALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> a -> Bool
     prop a b c = c *^ (a ^+^ b) == (c*^a) ^+^ (c*^b)
 
-testsNegateVector :: TestTree
-testsNegateVector = testProperty "Negation is inverse under ^+^"
-  TESTALLVECTORS(prop)
+prop_NegateVector :: Property
+prop_NegateVector = ALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> Bool
     prop a = (a ^+^ negated a) == zero
 
 tests :: [TestTree]
 tests =
-  [ testsLRScalarProduct
-  , testsDistScalarR
-  , testsDistScalarL
-  , testsNegateVector
-  , testsAddAssoc
-  , testsAddCommut
+  [ testProperty "Left and right scalar product are equal" prop_LRScalarProduct
+  , testProperty "Right scalar are distributive over vector addition" prop_DistScalarR
+  , testProperty "Left scalar are distributive over vector addition" prop_DistScalarL
+  , testProperty "Negation is inverse under ^+^" prop_NegateVector
+  , testProperty "associativity of ^+^" prop_AddAssoc
+  , testProperty "commutativity of ^+^" prop_AddCommut
   ]
