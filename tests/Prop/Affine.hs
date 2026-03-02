4787fc306991160d997d66653350c41edd186b79
Axel Flordal <axel@flordal.net>
2026-03-02 22:58:54 +0100

    Update affine

--- tests/Prop/Affine.hs
+++ tests/Prop/Affine.hs
@@ -2,7 +2,6 @@ module Prop.Affine (tests) where
 
 import Linear.Affine (Affine(..))
 import Linear.V3 (V3)
-import Linear.Vector ((^+^))
 import Prop.V3 ()
 import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck (testProperty)
@@ -11,14 +10,14 @@ prop_1 :: V3 Rational -> V3 Rational -> Bool
 prop_1 a b = a .+^ (b .-. a) == b
 
 prop_2 :: V3 Rational -> V3 Rational -> V3 Rational -> Bool
-prop_2 a u v = (a .+^ u) .+^ v == a .+^ (u ^+^ v)
+prop_2 a u v = (a .+^ u) .+^ v == a .+^ (u .+^ v)
 
 prop_3 :: V3 Rational -> V3 Rational -> V3 Rational -> Bool
 prop_3 a b v = (a .-. b) .+^ v == (a .+^ v) .-. b
 
 tests :: [TestTree]
 tests =
-  [ testProperty "prop 1" prop_1
-  , testProperty "prop 2" prop_2
-  , testProperty "prop 3" prop_3
+  [ testProperty "a .+^ (b .-. a) == b" prop_1
+  , testProperty "(a .+^ u) .+^ v == a .+^ (u .+^ v) " prop_2
+  , testProperty "(a .-. b) .+^ v == (a .+^ v) .-. b " prop_3
   ]
