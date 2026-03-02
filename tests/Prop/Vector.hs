babd9e83ac465b6c62c240de88ccfd268956e90f
Axel Flordal <axel@flordal.net>
2026-03-02 17:25:59 +0100

    Switch vector to Type application and macro test

--- tests/Prop/Vector.hs
+++ tests/Prop/Vector.hs
@@ -1,76 +1,72 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE CPP #-}
+
 module Prop.Vector (tests) where
 
 import Linear.V1 (V1)
 import Linear.V2 (V2)
 import Linear.V3 (V3)
 import Linear.V4 (V4)
-import Linear.Vector (negated, zero, (*^), (^*), (^+^))
+import Linear.Vector (Additive(..), negated, zero, (*^), (^*))
 import Prop.V1 ()
 import Prop.V2 ()
 import Prop.V3 ()
 import Prop.V4 ()
-import Test.Tasty (TestTree, testGroup)
+import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck (testProperty)
+import Test.QuickCheck ((.&&.))
+
+-- This macro avoids having these lines in all tests that
+-- want to run on all vectors. It is a bit evil but we think
+-- this is the simplest solution.
+#define TESTALLVECTOR(testname) \
+( testname @V1 @Rational .&&. \
+  testname @V2 @Rational .&&. \
+  testname @V3 @Rational .&&. \
+  testname @V4 @Rational \
+)
 
 testsAddAssoc :: TestTree
-testsAddAssoc = testGroup "associativity of ^+^"
-  [ testProperty "V1" (prop_addassoc :: V1 Rational -> V1 Rational -> V1 Rational -> Bool)
-  , testProperty "V2" (prop_addassoc :: V2 Rational -> V2 Rational -> V2 Rational -> Bool)
-  , testProperty "V3" (prop_addassoc :: V3 Rational -> V3 Rational -> V3 Rational -> Bool)
-  , testProperty "V4" (prop_addassoc :: V4 Rational -> V4 Rational -> V4 Rational -> Bool)
-  ]
+testsAddAssoc = testProperty "associativity of ^+^"
+  TESTALLVECTOR(prop)
   where
-      prop_addassoc a b c = ((a ^+^ b) ^+^ c) == (a ^+^ (b ^+^ c))
+    prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> v a -> Bool
+    prop a b c = ((a ^+^ b) ^+^ c) == (a ^+^ (b ^+^ c))
 
 testsAddCommut :: TestTree
-testsAddCommut = testGroup "commutativity of ^+^"
-  [ testProperty "V1" (prop_addcommut :: V1 Rational -> V1 Rational -> Bool)
-  , testProperty "V2" (prop_addcommut :: V2 Rational -> V2 Rational -> Bool)
-  , testProperty "V3" (prop_addcommut :: V3 Rational -> V3 Rational -> Bool)
-  , testProperty "V4" (prop_addcommut :: V4 Rational -> V4 Rational -> Bool)
-  ]
+testsAddCommut = testProperty "commutativity of ^+^"
+  TESTALLVECTOR(prop)
   where
-    prop_addcommut a b = (a ^+^ b) == (b ^+^ a)
+    prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> Bool
+    prop a b = (a ^+^ b) == (b ^+^ a)
 
 testsLRScalarProduct :: TestTree
-testsLRScalarProduct = testGroup "Left and right scalar product are equal"
-  [ testProperty "V1" (prop :: V1 Rational -> Rational -> Bool)
-  , testProperty "V2" (prop :: V2 Rational -> Rational -> Bool)
-  , testProperty "V3" (prop :: V3 Rational -> Rational -> Bool)
-  , testProperty "V4" (prop :: V4 Rational -> Rational -> Bool)
-  ]
+testsLRScalarProduct = testProperty "Left and right scalar product are equal"
+  TESTALLVECTOR(prop)
   where
+    prop :: (Eq (v a), Functor v, Num a) => v a -> a -> Bool
     prop v a = v ^* a == a *^ v
 
 testsDistScalarR :: TestTree
-testsDistScalarR = testGroup "Right scalar are distributive over vector addition"
-  [ testProperty "V1" (prop :: V1 Rational -> V1 Rational -> Rational -> Bool)
-  , testProperty "V2" (prop :: V2 Rational -> V2 Rational -> Rational -> Bool)
-  , testProperty "V3" (prop :: V3 Rational -> V3 Rational -> Rational -> Bool)
-  , testProperty "V4" (prop :: V4 Rational -> V4 Rational -> Rational -> Bool)
-  ]
+testsDistScalarR = testProperty "Right scalar are distributive over vector addition"
+  TESTALLVECTOR(prop)
   where
+    prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> a -> Bool
     prop a b c = (a ^+^ b) ^* c == (a^*c) ^+^ (b^*c)
 
 testsDistScalarL :: TestTree
-testsDistScalarL = testGroup "Left scalar are distributive over vector addition"
-  [ testProperty "V1" (prop :: V1 Rational -> V1 Rational -> Rational -> Bool)
-  , testProperty "V2" (prop :: V2 Rational -> V2 Rational -> Rational -> Bool)
-  , testProperty "V3" (prop :: V3 Rational -> V3 Rational -> Rational -> Bool)
-  , testProperty "V4" (prop :: V4 Rational -> V4 Rational -> Rational -> Bool)
-  ]
+testsDistScalarL = testProperty "Left scalar are distributive over vector addition"
+  TESTALLVECTOR(prop)
   where
+    prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> a -> Bool
     prop a b c = c *^ (a ^+^ b) == (c*^a) ^+^ (c*^b)
 
 testsNegateVector :: TestTree
-testsNegateVector = testGroup "Negation is inverse under ^+^"
-  [ testProperty "V1" (prop :: V1 Rational -> Bool)
-  , testProperty "V2" (prop :: V2 Rational -> Bool)
-  , testProperty "V3" (prop :: V3 Rational -> Bool)
-  , testProperty "V4" (prop :: V4 Rational -> Bool)
-  ]
+testsNegateVector = testProperty "Negation is inverse under ^+^"
+  TESTALLVECTOR(prop)
   where
-    prop a = a ^+^ negated a == zero
+    prop :: (Eq (v a), Additive v, Num a) => v a -> Bool
+    prop a = (a ^+^ negated a) == zero
 
 tests :: [TestTree]
 tests =
