b3787cc3991b6d8ff2b3097ca8954613500a6f53
Anton Nordbeck <anton.nordbeck@gmail.com>
2026-03-02 11:05:30 +0100

    added prop test arising from Additive for vectors

--- tests/Prop/Vector.hs
+++ tests/Prop/Vector.hs
@@ -1,19 +1,83 @@
 module Prop.Vector (tests) where
 
+import Linear.V1 (V1)
+import Linear.V2 (V2)
 import Linear.V3 (V3)
+import Linear.V4 (V4)
 import Linear.Vector (negated, zero, (*^), (^*), (^+^))
+import Prop.V1 ()
+import Prop.V2 ()
 import Prop.V3 ()
-import Test.Tasty (TestTree)
+import Prop.V4 ()
+import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.QuickCheck (testProperty)
 
-prop_lr_scalarproduct :: V3 Rational -> Rational -> Bool
-prop_lr_scalarproduct v a = v ^* a == a *^ v
+testsAddAssoc :: TestTree
+testsAddAssoc = testGroup "associativity of ^+^"
+  [ testProperty "V1" (prop_addassoc :: V1 Rational -> V1 Rational -> V1 Rational -> Bool)
+  , testProperty "V2" (prop_addassoc :: V2 Rational -> V2 Rational -> V2 Rational -> Bool)
+  , testProperty "V3" (prop_addassoc :: V3 Rational -> V3 Rational -> V3 Rational -> Bool)
+  , testProperty "V4" (prop_addassoc :: V4 Rational -> V4 Rational -> V4 Rational -> Bool)
+  ]
+  where
+      prop_addassoc a b c = ((a ^+^ b) ^+^ c) == (a ^+^ (b ^+^ c))
 
-prop_negate_vector :: V3 Rational -> Bool
-prop_negate_vector a = a ^+^ negated a == zero
+testsAddCommut :: TestTree
+testsAddCommut = testGroup "commutativity of ^+^"
+  [ testProperty "V1" (prop_addcommut :: V1 Rational -> V1 Rational -> Bool)
+  , testProperty "V2" (prop_addcommut :: V2 Rational -> V2 Rational -> Bool)
+  , testProperty "V3" (prop_addcommut :: V3 Rational -> V3 Rational -> Bool)
+  , testProperty "V4" (prop_addcommut :: V4 Rational -> V4 Rational -> Bool)
+  ]
+  where
+    prop_addcommut a b = (a ^+^ b) == (b ^+^ a)
+
+testsLRScalarProduct :: TestTree
+testsLRScalarProduct = testGroup "Left and right scalar product are equal"
+  [ testProperty "V1" (prop :: V1 Rational -> Rational -> Bool)
+  , testProperty "V2" (prop :: V2 Rational -> Rational -> Bool)
+  , testProperty "V3" (prop :: V3 Rational -> Rational -> Bool)
+  , testProperty "V4" (prop :: V4 Rational -> Rational -> Bool)
+  ]
+  where
+    prop v a = v ^* a == a *^ v
+
+testsDistScalarR :: TestTree
+testsDistScalarR = testGroup "Right scalar are distributive over vector addition"
+  [ testProperty "V1" (prop :: V1 Rational -> V1 Rational -> Rational -> Bool)
+  , testProperty "V2" (prop :: V2 Rational -> V2 Rational -> Rational -> Bool)
+  , testProperty "V3" (prop :: V3 Rational -> V3 Rational -> Rational -> Bool)
+  , testProperty "V4" (prop :: V4 Rational -> V4 Rational -> Rational -> Bool)
+  ]
+  where
+    prop a b c = (a ^+^ b) ^* c == (a^*c) ^+^ (b^*c)
+
+testsDistScalarL :: TestTree
+testsDistScalarL = testGroup "Left scalar are distributive over vector addition"
+  [ testProperty "V1" (prop :: V1 Rational -> V1 Rational -> Rational -> Bool)
+  , testProperty "V2" (prop :: V2 Rational -> V2 Rational -> Rational -> Bool)
+  , testProperty "V3" (prop :: V3 Rational -> V3 Rational -> Rational -> Bool)
+  , testProperty "V4" (prop :: V4 Rational -> V4 Rational -> Rational -> Bool)
+  ]
+  where
+    prop a b c = c *^ (a ^+^ b) == (c*^a) ^+^ (c*^b)
+
+testsNegateVector :: TestTree
+testsNegateVector = testGroup "Negation is inverse under ^+^"
+  [ testProperty "V1" (prop :: V1 Rational -> Bool)
+  , testProperty "V2" (prop :: V2 Rational -> Bool)
+  , testProperty "V3" (prop :: V3 Rational -> Bool)
+  , testProperty "V4" (prop :: V4 Rational -> Bool)
+  ]
+  where
+    prop a = a ^+^ negated a == zero
 
 tests :: [TestTree]
 tests =
-  [ testProperty "Left and right scalar product are equal" prop_lr_scalarproduct
-  , testProperty "Negation is inverse under ^+^" prop_negate_vector
+  [ testsLRScalarProduct
+  , testsDistScalarR
+  , testsDistScalarL
+  , testsNegateVector
+  , testsAddAssoc
+  , testsAddCommut
   ]
