2729238d50afd20f211df0c105ba7827c5057d89
Axel Flordal <axel@flordal.net>
2026-03-02 17:27:08 +0100

    Add S to TESTALLVECTORS

--- tests/Prop/Vector.hs
+++ tests/Prop/Vector.hs
@@ -19,7 +19,7 @@ import Test.QuickCheck ((.&&.))
 -- This macro avoids having these lines in all tests that
 -- want to run on all vectors. It is a bit evil but we think
 -- this is the simplest solution.
-#define TESTALLVECTOR(testname) \
+#define TESTALLVECTORS(testname) \
 ( testname @V1 @Rational .&&. \
   testname @V2 @Rational .&&. \
   testname @V3 @Rational .&&. \
@@ -28,42 +28,42 @@ import Test.QuickCheck ((.&&.))
 
 testsAddAssoc :: TestTree
 testsAddAssoc = testProperty "associativity of ^+^"
-  TESTALLVECTOR(prop)
+  TESTALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> v a -> Bool
     prop a b c = ((a ^+^ b) ^+^ c) == (a ^+^ (b ^+^ c))
 
 testsAddCommut :: TestTree
 testsAddCommut = testProperty "commutativity of ^+^"
-  TESTALLVECTOR(prop)
+  TESTALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> Bool
     prop a b = (a ^+^ b) == (b ^+^ a)
 
 testsLRScalarProduct :: TestTree
 testsLRScalarProduct = testProperty "Left and right scalar product are equal"
-  TESTALLVECTOR(prop)
+  TESTALLVECTORS(prop)
   where
     prop :: (Eq (v a), Functor v, Num a) => v a -> a -> Bool
     prop v a = v ^* a == a *^ v
 
 testsDistScalarR :: TestTree
 testsDistScalarR = testProperty "Right scalar are distributive over vector addition"
-  TESTALLVECTOR(prop)
+  TESTALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> a -> Bool
     prop a b c = (a ^+^ b) ^* c == (a^*c) ^+^ (b^*c)
 
 testsDistScalarL :: TestTree
 testsDistScalarL = testProperty "Left scalar are distributive over vector addition"
-  TESTALLVECTOR(prop)
+  TESTALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> v a -> a -> Bool
     prop a b c = c *^ (a ^+^ b) == (c*^a) ^+^ (c*^b)
 
 testsNegateVector :: TestTree
 testsNegateVector = testProperty "Negation is inverse under ^+^"
-  TESTALLVECTOR(prop)
+  TESTALLVECTORS(prop)
   where
     prop :: (Eq (v a), Additive v, Num a) => v a -> Bool
     prop a = (a ^+^ negated a) == zero
