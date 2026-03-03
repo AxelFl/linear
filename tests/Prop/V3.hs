83c68a89ed31c9d0c6beb3a282006f3405e261ba
Anton Nordbeck <anton.nordbeck@gmail.com>
2026-03-03 11:39:03 +0100

    added triple product tests

--- tests/Prop/V3.hs
+++ tests/Prop/V3.hs
@@ -2,7 +2,8 @@
 module Prop.V3 (tests) where
 
 import Linear.V3 (V3(..), cross)
-import Linear.Vector (zero)
+import Linear.Vector (zero, (*^), (^-^))
+import Linear.Metric (dot)
 import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck (testProperty)
 import Test.QuickCheck (Arbitrary(..))
@@ -19,9 +20,17 @@ prop_selfcross a = a `cross` a == zero
 prop_distcross :: V3 Rational -> V3 Rational -> V3 Rational -> Bool
 prop_distcross a b c = (a + b) `cross` c == (a `cross` c) + (b `cross` c)
 
+prop_scalartripleprod :: V3 Rational -> V3 Rational -> V3 Rational -> Bool
+prop_scalartripleprod a b c = a `dot` (b `cross` c) == b `dot` (c `cross` a) && a `dot` (b `cross` c) == c `dot` (a `cross` b)
+
+prop_vectortripleprod :: V3 Rational -> V3 Rational -> V3 Rational -> Bool
+prop_vectortripleprod a b c = (a `cross` (b `cross` c)) == ((a `dot` c) *^ b) ^-^ ((a `dot` b) *^ c)
+
 tests :: [TestTree]
 tests = 
   [ testProperty "a x b == - (b x a)" prop_crossinv
   , testProperty "a x a == 0" prop_selfcross
   , testProperty "(a + b) x c == a x c + b x c" prop_distcross
+  , testProperty "Scalar triple product" prop_scalartripleprod
+  , testProperty "Vector triple product" prop_vectortripleprod
   ]
